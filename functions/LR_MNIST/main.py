f9859fb8d53aa8c4d6c5dfd849705901600c1bb7
ganshaoduo <12163418+ganshaoduo@users.noreply.github.com>
2019-10-17 13:41:26 +0200

    add invocation

--- functions/LR_MNIST/main.py
+++ functions/LR_MNIST/main.py
@@ -1,5 +1,21 @@
 from dataset_utils import preprocess, partition_training_data
 import os
+import boto3
+
+class LogisticRegression(torch.nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LogisticRegression, self).__init__()
+        self.linear = torch.nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        outputs = self.linear(x)
+        return outputs
+
+input_dim = 784
+output_dim = 10
+model = LogisticRegression(input_dim, output_dim)
+
+
 
 def handler(event,context):
 	
@@ -16,4 +32,18 @@ def handler(event,context):
 
     # shuffle and partition training data
 	partition_training_data(bucket_name, data_path, training_x, training_y, num_workers)
+
+	# invoke functions 
+	payload = dict()
+	payload['dataset'] = dataset_name
+	payload['data_bucket'] = bucket_name
+	payload['model_bucket'] = 'model_bucket'
+	payload['num_workers'] = num_workers
+
+	# invoke functions
+	for i in range(num_workers):
+		payload['rank'] = i
+		payload['keys_training_data'] = 'training_{}'.pt.format(i)
+		lambda_client = boto3.client('lambda')
+		lambda_client.invoke(FunctionName='...', InvocationType='Event', Payload=json.dumps(payload))
 
