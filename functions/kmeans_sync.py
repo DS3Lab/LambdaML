c5be0856cc8139a39dd9774d5cc780901d0ef342
Fanlin Wang <mywfl@outlook.com>
2019-10-24 18:56:02 +0200

    Minor fixes.

--- functions/kmeans_sync.py
+++ functions/kmeans_sync.py
@@ -1,15 +1,14 @@
 import time
 import urllib.parse
 import logging
-import numpy as np
+import boto3
 
-from s3.list_objects import list_bucket_objects
-from s3.get_object import get_object
-from s3.put_object import put_object
 from sync.sync_centroids import *
+from s3.get_object import get_object_or_wait
+from s3.get_object import get_object
+from data_loader.LibsvmDataset import DenseLibsvmDataset
 
 from model.Kmeans import Kmeans
-from data_loader.LibsvmDataset import DenseLibsvmDataset2
 from sync.sync_meta import SyncMeta
 
 # setting
@@ -21,72 +20,74 @@ logger.setLevel(logging.INFO)
 worker_cent_bucket = "worker_centroids"
 avg_cent_bucket = "avg_centroids"
 
-def process_centroid(centroid_byte, num_clusters):
-	cent = np.frombuffer(centroid_byte)
-	cent_size = cent.shape[0]
-	return cent.reshape(int(cent_size/num_clusters), num_clusters)
+
+def process_centroid(centroid_byte, n):
+    cent = np.frombuffer(centroid_byte)
+    cent_size = cent.shape[0]
+    return cent.reshape(int(cent_size/n), n)
+
 
 def lambda_handler(event, context):
-	startTs = time.time()
+    startTs = time.time()
 
-	# Reading data from S3
-	bucket_name = event['bucket_name']
-	key = urllib.parse.unquote_plus(event['key'], encoding='utf-8')
+    # Reading data from S3
+    bucket_name = event['bucket_name']
+    key = urllib.parse.unquote_plus(event['key'], encoding='utf-8')
 
-	logger.info(f'Reading training data from bucket = {bucket_name}, key = {key}')
+    logger.info(f'Reading training data from bucket = {bucket_name}, key = {key}')
 
-	key_splits = key.split("_")
-	worker_index = int(key_splits[0])
+    key_splits = key.split("_")
+    worker_index = int(key_splits[0])
 
-	num_worker = int(key_splits[1])
-	sync_meta = SyncMeta(worker_index, num_worker)
-	logger.info("synchronization meta {}".format(sync_meta.__str__()))
-	centroids = np.array()
+    num_worker = int(key_splits[1])
+    sync_meta = SyncMeta(worker_index, num_worker)
+    logger.info("synchronization meta {}".format(sync_meta.__str__()))
+    centroids = np.array()
 
-	# read file from s3
-	if (worker_index == 0):
-		file = get_object(bucket, key).read().decode('utf-8').split("\n")
-		dataset_np = DenseLibsvmDataset(file).ins_list_np
-		centroids = dataset_np[0:num_clusters]
-		put_object(avg_cent_bucket, "initial", cent.tobytes())
-	else:
-		cent = get_object_or_wait(avg_cent_bucket, "initial", 100).read()
-		if (cent is None):
-			logger.error("timeout for waiting initial centorids")
-			return 0
-		centroids = process_centroid(cent)
-		file = get_object(bucket, key).read().decode('utf-8').split("\n")
-		dataset_np = DenseLibsvmDataset(file).ins_list_np
-		logger.info("read data cost {} s".format(time.time() - startTs))
+    # read file from s3
+    if worker_index == 0:
+        file = get_object(bucket_name, key).read().decode('utf-8').split("\n")
+        dataset_np = DenseLibsvmDataset(file).ins_list_np
+        centroids = dataset_np[0:num_clusters]
+        put_object(avg_cent_bucket, "initial", centroids.tobytes())
+    else:
+        cent = get_object_or_wait(avg_cent_bucket, "initial", 100).read()
+        if cent is None:
+            logger.error("timeout for waiting initial centorids")
+            return 0
+        centroids = process_centroid(cent)
+        file = get_object(bucket_name, key).read().decode('utf-8').split("\n")
+        dataset_np = DenseLibsvmDataset(file).ins_list_np
+        logger.info("read data cost {} s".format(time.time() - startTs))
 
-	parse_start = time.time()
-	logger.info(f"Dataset size: {dataset_np.shape}")
-	logger.info("parse data cost {} s".format(time.time() - parse_start))
+    parse_start = time.time()
+    logger.info(f"Dataset size: {dataset_np.shape}")
+    logger.info("parse data cost {} s".format(time.time() - parse_start))
 
-	preprocess_start = time.time()
-	s3 = boto3.client('s3')
+    preprocess_start = time.time()
+    s3 = boto3.client('s3')
 
-	# Check whether the dimension of the centorids is correct
-	if centroids.shape[1] != num_clusters:
-		logger.error(f"current centroids has shape {centroids.shape}. Expected {num_clusters}")
+    # Check whether the dimension of the centorids is correct
+    if centroids.shape[1] != num_clusters:
+        logger.error(f"current centroids has shape {centroids.shape}. Expected {num_clusters}")
 
-	# training
-	for epoch in range(num_epochs):
-		if (epoch != 0):
-			last_epoch = epoch - 1
-			cent = get_object_or_wait(avg_cent_bucket, f"avg_{last_epoch}", 100)
-			if (cent is None):
-				logger.error(f"timeout for waiting centorids for {epoch}")
-				return 0
-			centroids = process_centroid(cent)
+    # training
+    for epoch in range(num_epochs):
+        if epoch != 0:
+            last_epoch = epoch - 1
+            cent = get_object_or_wait(avg_cent_bucket, f"avg_{last_epoch}", 100)
+            if cent is None:
+                logger.error(f"timeout for waiting centorids for {epoch}")
+                return 0
+            centroids = process_centroid(cent)
 
-		model = Kmeans(dataset_np, centroids)
-		logger.info(f"Epoch = {epoch} Error = {model.error}")
+        model = Kmeans(dataset_np, centroids)
+        logger.info(f"Epoch = {epoch} Error = {model.error}")
 
-		sync_start = time.time()
-		put_object(worker_cent_bucket, f"{worker_index}_{epoch}", model.centroids.tobytes())
+        sync_start = time.time()
+        put_object(worker_cent_bucket, f"{worker_index}_{epoch}", model.centroids.tobytes())
 
-		if worker_index == 0:
-			compute_average_centroids(avg_cent_bucket, worker_cent_bucket, num_workers, shape, epoch);
+        if worker_index == 0:
+            compute_average_centroids(avg_cent_bucket, worker_cent_bucket, num_worker, centroids.shape, epoch);
 
-		logger.info("synchronization cost {} s".format(time.time() - sync_start))
+        logger.info("synchronization cost {} s".format(time.time() - sync_start))
--- model/Kmeans.py
+++ model/Kmeans.py
@@ -1,35 +1,31 @@
-import math
 import numpy as np
 import logging
-
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
 class Kmeans:
-	def __init__(self, data, centroids, error=np.iinfo(np.int16).max):
-		self.X = data
-		self.centroids = centroids
-		self.error = error
+    def __init__(self, data, centroids, error=np.iinfo(np.int16).max):
+        self.X = data
+        self.centroids = centroids
+        self.error = error
 
-	def l2_dist(self, a, b, ax=1):
-		return np.linalg.norm(a - b, axis=ax)
+    def l2_dist(self, a, b, ax=1):
+        return np.linalg.norm(a - b, axis=ax)
 
-	def closest_centroid(self, points, centroids):
-	    """returns an array containing the index to the nearest centroid for each point"""
-	    distances = np.sqrt(((points - centroids[:, np.newaxis])**2).sum(axis=2))
-	    return np.argmin(distances, axis=0)
+    def closest_centroid(self, points, centroids):
+        """returns an array containing the index to the nearest centroid for each point"""
+        distances = np.sqrt(((points - centroids[:, np.newaxis]) ** 2).sum(axis=2))
+        return np.argmin(distances, axis=0)
 
+    def move_centroids(self, points, closest, centroids):
+        """returns the new centroids assigned from the points closest to them"""
+        return np.array([points[closest == k].mean(axis=0) for k in range(centroids.shape[0])])
 
-	def move_centroids(self, points, closest, centroids):
-	    """returns the new centroids assigned from the points closest to them"""
-	    return np.array([points[closest==k].mean(axis=0) for k in range(centroids.shape[0])])
-
-
-	def find_nearest_cluster(self):
-		closest = closest_centroid(self.X, self.centroids)
-		new_centroids_vec = move_centroids(self.X, closest, self.centroids)
-		self.error = l2_dist(self.centroids, new_centroids_vec, None)
-		self.centroids = new_centroids_vec
-		return
+    def find_nearest_cluster(self):
+        closest = self.closest_centroid(self.X, self.centroids)
+        new_centroids_vec = self.move_centroids(self.X, closest, self.centroids)
+        self.error = self.l2_dist(self.centroids, new_centroids_vec)
+        self.centroids = new_centroids_vec
+        return
