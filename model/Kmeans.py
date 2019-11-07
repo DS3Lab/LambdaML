881d82ef558619e50103a928a973a9debfbb5780
Fanlin Wang <mywfl@outlook.com>
2019-11-07 09:48:57 +0100

    Compute centroids in batch to avoid OOM.

--- model/Kmeans.py
+++ model/Kmeans.py
@@ -16,14 +16,43 @@ class Kmeans:
 
     def closest_centroid(self, points, centroids):
         """returns an array containing the index to the nearest centroid for each point"""
-        distances = np.sqrt(((points - centroids[:, np.newaxis]) ** 2).sum(axis=2))
-        return np.argmin(distances, axis=0)
+        batch_size = 5000
+        remaining = points.shape[0]
+        current_batch = 0
+        argmin_dist = []
+
+        while True:
+            print(remaining)
+            if (remaining <= batch_size):
+                dist = np.full((remaining, centroids.shape[0]), np.inf)
+                for i in range(centroids.shape[0]):
+                    dist[:, i] = np.sum(np.square(points[current_batch:current_batch + remaining] - centroids[i, :]),
+                                        axis=1)
+                argmin_dist.append(np.argmin(dist, axis=1))
+                break
+            else:
+                dist = np.full((batch_size, centroids.shape[0]), np.inf)
+                for i in range(centroids.shape[0]):
+                    dist[:, i] = np.sum(np.square(points[current_batch:current_batch + batch_size] - centroids[i, :]),
+                                        axis=1)
+                argmin_dist.append(np.argmin(dist, axis=1))
+                current_batch += batch_size
+                remaining -= batch_size
+
+        res = argmin_dist[0]
+        print(len(argmin_dist))
+        for i in range(1, len(argmin_dist)):
+            print(argmin_dist[i].shape)
+            res = np.concatenate((res, argmin_dist[i]), axis=None)
+        return res
 
     def move_centroids(self, points, closest, centroids):
         """returns the new centroids assigned from the points closest to them"""
-        return np.array([points[closest == k].mean(axis=0) for k in range(centroids.shape[0])])
+        x = np.array([points[closest == k].mean(axis=0) for k in range(centroids.shape[0])])
+        return np.nan_to_num(x)
 
     def find_nearest_cluster(self):
+        # centroids.shape: nr_clusters, data_dim
         closest = self.closest_centroid(self.X, self.centroids)
         new_centroids_vec = self.move_centroids(self.X, closest, self.centroids)
         self.error = self.l2_dist(self.centroids, new_centroids_vec)
