a3068f5d170d56506ae8f0e816a2f5428e109676
yuel5304 <yuel5304@gmail.com>
2019-11-19 22:25:45 +0100

    multi-layer model sync via redis

--- sync/sync_grad_redis.py
+++ sync/sync_grad_redis.py
@@ -9,6 +9,77 @@ from elasticache.Redis.set_object import hset_object
 from elasticache.Redis.delete_keys import hdelete_keys
 from elasticache.Redis.counter import hcounter
 
+import pickle
+
+def merge_w_b_layers(endpoint, bucket_name, num_workers, prefix):
+    #whatever merges w/b grads or model
+    
+    num_files = 0
+    merged_value = []
+    
+    while num_files < num_workers:
+        objects = hlist_keys(endpoint, bucket_name)
+        if objects is not None:
+            for obj in objects:
+                file_key = bytes.decode(obj)
+                data_bytes = hget_object(endpoint, bucket_name, file_key)
+                data = pickle.loads(data_bytes)
+                
+                for i in range(len(data)):
+                    if num_files == 0:
+                        merged_value.append(np.zeros(data[i].shape, dtype=data[i].dtype))
+                        
+                    merged_value[i] = merged_value[i] + data[i]
+                
+                num_files = num_files + 1
+                hdelete_keys(endpoint, bucket_name, [file_key])
+
+    # average weights
+    if prefix == 'w_':
+        merged_value = [value / float(num_workers) for value in merged_value]
+
+    return merged_value
+
+def put_merged_w_b_layers(endpoint, bucket_name, merged_value, prefix, file_postfix):
+    # print('put merged weight {} to bucket {}'.format(w_prefix + file_postfix, bucket_name))
+    hset_object(endpoint, bucket_name, prefix + file_postfix, pickle.dumps(merged_value))
+    # print('put merged bias {} to bucket {}'.format(b_prefix + file_postfix, bucket_name))
+    # put_object(bucket_name, b_prefix + file_postfix, b.tobytes())
+
+
+def get_merged_w_b_layers(endpoint, bucket_name, prefix, file_postfix):
+    # print('get merged weight {} in bucket {}'.format(w_prefix + file_postfix, bucket_name))
+    merged_value = hget_object_or_wait(endpoint, bucket_name, prefix + file_postfix, 0.1).read()
+    merged_value_np = pickle.loads(merged_value)
+    # merged_value_np = np.frombuffer(merged_value, dtype=dtype).reshape(dshape)
+
+    return merged_value_np
+
+def delete_expired(bucket_name, cur_epoch, cur_batch, prefix):
+    objects = list_bucket_objects(bucket_name)
+    if objects is not None:
+        for obj in objects:
+            file_key = urllib.parse.unquote_plus(obj["Key"], encoding='utf-8')
+            if file_key.startswith(prefix):
+                key_splits = file_key.split("_")
+                key_batch = int(key_splits[-1])
+                key_epoch = int(key_splits[-2])
+                if key_epoch < cur_epoch or (key_epoch == cur_epoch and key_batch < cur_batch):
+                    print("delete object {} in bucket {}".format(file_key, bucket_name))
+                    delete_object(bucket_name, file_key)
+
+
+
+
+
+
+
+
+
+
+
+#mergence for single file
+
 def merge_np_bytes(endpoint, bucket_name, num_workers, dtype, shape):
     num_files = 0
     sum_arr = np.zeros(shape, dtype=dtype)
@@ -61,43 +132,6 @@ def merge_w_b_grads(endpoint, bucket_name, num_workers,
             objects = hlist_keys(endpoint,bucket_name)
             #print("the keys being deleted = {}".format(objects))
         
-        
-    return w_grad_sum, b_grad_sum
-
-def merge_w_b_grads_layers(endpoint, bucket_name, num_workers,
-                    dtype, w_shape, b_shape,
-                    w_grad_prefix="w_grad_", b_grad_prefix="b_grad"):
-    num_w_files = 0
-    num_b_files = 0
-    w_grad_sum = []
-    b_grad_sum = []
-    for i in range(len(w_shape)):
-        w_grad_sum.append(np.zeros(w_shape[0], dtype=dtype))
-        b_grad_sum.append(np.zeros(b_shape[0], dtype=dtype))
-    layers = len(w_shape)
-    while num_w_files < layers*num_workers or num_b_files < layers*num_workers:
-       
-        objects = hlist_keys(endpoint,bucket_name)
-        while objects is not None:
-            for obj in objects:
-                file_key = bytes.decode(obj)
-                #print("the name of the file being processed = {}".format(file_key))
-                bytes_data = np.fromstring(hget_object(endpoint, bucket_name, file_key), dtype)
-                if file_key.startswith(w_grad_prefix):
-                    layer = file_key.split("_")[2]
-                    w_grad = bytes_data.reshape(w_shape[layer])
-                    print("merge the {}-th weight grad {} in bucket {} = {}".format(num_w_files, file_key, bucket_name, w_grad[0][:5]))
-                    w_grad_sum[layer] = w_grad_sum[layer] + w_grad
-                    num_w_files = num_w_files+ 1
-                elif file_key.startswith(b_grad_prefix):
-                    b_grad = bytes_data.reshape(b_shape[layer])
-                    print("merge the {}-th bias grad {} in bucket {} = {}".format(num_b_files, file_key, bucket_name, b_grad))
-                    b_grad_sum[layer] = b_grad_sum[layer] + b_grad
-                    num_b_files = num_b_files + 1
-                #the processed keys are deleted at the stage of mergence
-                hdelete_keys(endpoint,bucket_name,[file_key])
-            objects = hlist_keys(endpoint,bucket_name)
-            #print("the keys being deleted = {}".format(objects))
         
     return w_grad_sum, b_grad_sum
 
