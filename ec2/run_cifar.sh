a808901eabd888bd161456bfbf18a4219d6ddf86
ganshaoduo <12163418+ganshaoduo@users.noreply.github.com>
2020-01-07 16:49:37 +0100

    Update run_cifar.sh

--- ec2/run_cifar.sh
+++ ec2/run_cifar.sh
@@ -17,3 +17,15 @@ for ((i=0; i<$world_size; i++)); do
                 ssh cluster-node00$i "source /home/ubuntu/envs/pytorch/bin/activate; cd /home/ubuntu/code_2.0/LambdaML/ec2/; python3.6 /home/ubuntu/code_2.0/LambdaML/ec2/cifar10.py --init-method tcp://172.31.0.225:24000 --rank $i --world-size $world_size --root /mnt --no-cuda --batch-size $batch_size_global" &
         fi
 done
+
+# if more than 10 nodes
+# for ((i=0; i<$world_size; i++)); do
+#         if [ $i -eq 0 ]; then
+#             source /home/ubuntu/envs/pytorch/bin/activate
+#             python3.6 /home/ubuntu/code_2.0/LambdaML/ec2/cifar10.py --init-method tcp://172.31.44.193:24000 --rank 0 --world-size $world_size --root /mnt --no-cuda --batch-size $batch_size_global &
+#         elif [ $i -le 9 ]; then
+#             ssh cluster20-node00$i "source /home/ubuntu/envs/pytorch/bin/activate; cd /home/ubuntu/code_2.0/LambdaML/ec2/; python3.6 /home/ubuntu/code_2.0/LambdaML/ec2/cifar10.py --init-method tcp://172.31.44.193:24000 --rank $i --world-size $world_size --root /mnt --no-cuda --batch-size $batch_size_global" &
+#         else
+#         	ssh cluster20-node0$i "source /home/ubuntu/envs/pytorch/bin/activate; cd /home/ubuntu/code_2.0/LambdaML/ec2/; python3.6 /home/ubuntu/code_2.0/LambdaML/ec2/cifar10.py --init-method tcp://172.31.44.193:24000 --rank $i --world-size $world_size --root /mnt --no-cuda --batch-size $batch_size_global" &
+#         fi
+# done
