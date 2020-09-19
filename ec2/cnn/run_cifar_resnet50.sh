aa1aea82ad436b862b1ff6d115878eecdb09019a
Shaoduo <ganshaoduo@gmail.com>
2020-09-19 20:57:13 +0200

    a

--- ec2/cnn/run_cifar_resnet50.sh
+++ ec2/cnn/run_cifar_resnet50.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
 world_size=$1
-# echo $world_size
 
-# batch_size_local=$2
-# echo $batch_size_local
-# batch_size is the global batch size
-# batch_size_global=`expr $world_size \* $batch_size_local`
+number_of_machines=$2
+cores_per_machine=$3
+master_ip=$4
 
-for ((i=0; i<$world_size; i++)); do
-  /home/ubuntu/anaconda3/envs/pytorch_p36/bin/python3 -u /home/ubuntu/LambdaML/ec2/cnn/cifar10_resnet50_grad_avg.py --rank $i --world-size $world_size --root /home/ubuntu/cifar10 --no-cuda &
+
+# one machine
+# for ((i=0; i<$world_size; i++)); do
+#   /home/ubuntu/anaconda3/envs/pytorch_p36/bin/python3 -u /home/ubuntu/LambdaML/ec2/cnn/cifar10_resnet50_grad_avg.py --init-method --rank $i --world-size $world_size --root /home/ubuntu/cifar10 --no-cuda &
+# done
+
+# multiple machines
+for ((i=0; i<$number_of_machines; i++)); do
+
+        if [ $i -eq 0 ]; then
+            for ((j=0; j<$cores_per_machine; j++)); do    
+                /home/ubuntu/anaconda3/envs/pytorch_p36/bin/python3 -u /home/ubuntu/LambdaML/ec2/cnn/cifar10_resnet50_grad_avg.py --init-method tcp://$master_ip --rank $j --world-size $world_size --root /home/ubuntu --no-cuda &
+            done
+        else
+            for ((p=0; p<$cores_per_machine; p++)); do
+
+                ssh worker$i "/home/ubuntu/anaconda3/envs/pytorch_p36/bin/python3 -u /home/ubuntu/LambdaML/ec2/cnn/cifar10_resnet50_grad_avg.py --init-method tcp://$master_ip --rank `expr $i \* $cores_per_machine + $p` --world-size $world_size --root /home/ubuntu --no-cuda" &
+            done
+        fi
 done
 
-# if more than 10 nodes
 # for ((i=0; i<$world_size; i++)); do
 #         if [ $i -eq 0 ]; then
 #             source /home/ubuntu/envs/pytorch/bin/activate
